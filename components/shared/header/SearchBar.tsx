8de5d8d740ab564032f82f6852949936cf7d1113
PoshX1z <xdante1357@gmail.com>
2025-04-21 12:22:06 +0700

    Update some codes

--- components/shared/header/SearchBar.tsx
+++ components/shared/header/SearchBar.tsx
@@ -1,19 +1,20 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
-import React from "react";
 
 const SearchBar = () => {
   return (
     <form method="GET" action="/search" className="flex items-center">
       <Input
-        className="flex-1 rounded-3xl bg-white text-black text-base"
-        placeholder="Search game..."
         name="q"
         type="search"
+        placeholder="Search game..."
+        className="flex-1 rounded-3xl bg-white text-black text-base"
       />
       <button
         type="submit"
-        className="bg-blue-500 p-2 rounded-3xl text-white cursor-pointer hover:bg-blue-600 transition duration-200 ease-in-out"
+        className="bg-blue-500 p-2 rounded-3xl text-white cursor-pointer hover:bg-blue-600 transition duration-200"
       >
         <SearchIcon />
       </button>
